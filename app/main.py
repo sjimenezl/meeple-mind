e8469bf7be0e18d81badb9df0b2c3f64b556a04a
Siomara Jimenez <sakuritita@gmail.com>
2025-07-07 16:36:25 -0600

    trying to fix context

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-from app.api.graphql import graphql_app
-from app.core.config import settings
+from fastapi.responses import HTMLResponse
+from strawberry.fastapi import GraphQLRouter
+from app.graphql.schema import schema
+from app.graphql.context import Context
+
+def custom_context_getter(request):
+    return Context(request)
+
+
+graphql_router = GraphQLRouter(
+    schema,
+    graphiql=False,
+    context_getter=custom_context_getter
+)
 
 app = FastAPI()
 
@@ -12,8 +24,40 @@ async def db_session_middleware(request, call_next):
     response = await call_next(request)
     return response
 
-# montas GraphQL en la ruta /graphql
-app.include_router(graphql_app, prefix="/api")
+from fastapi.responses import HTMLResponse
 
-for route in app.routes:
-    print(route.path)
+@app.get("/api/playground", response_class=HTMLResponse)
+async def playground():
+    return """
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset=utf-8/>
+        <title>GraphQL Playground</title>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css" />
+        <script src="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js"></script>
+      </head>
+      <body>
+        <div id="root" />
+        <script>
+          window.addEventListener('load', function (event) {
+            GraphQLPlayground.init(document.getElementById('root'), {
+              endpoint: '/api/graphql',
+              headers: {
+                "Content-Type": "application/json"
+              },
+              tabs: [
+                {
+                  endpoint: "http://localhost:8000/api/graphql"
+                }
+              ]
+            })
+          })
+        </script>
+      </body>
+    </html>
+    """
+
+
+
+app.include_router(graphql_router, prefix="/api/graphql")
